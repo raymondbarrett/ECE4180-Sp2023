76de017bffd04e45e4e9b1604871e289cc589cd1
mshakula <matvey@gatech.edu>
2023-01-22 00:41:04 -0500

    Lab1:
    Added base project structure.

---
+++ Lab1/src/main.cpp
@@ -0,0 +1,14 @@
+/// \file src/main.cpp
+/// \brief Main entry point.
+///
+
+#if !defined(__cplusplus) || __cplusplus < 201402L
+#error "C++14 (max supported by online compiler) required."
+#endif
+
+/// \brief Main entrypoint.
+int
+main()
+{
+  return 0;
+}
