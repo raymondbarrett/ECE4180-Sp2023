4346fb374320ed8191ac63ad9b1ec03f5f398e7b
Raymond Barrett <rbarrett38@gatech.edu>
2023-02-28 17:18:53 -0500

    added bluetooth threading

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,34 @@
 
 namespace {
 
+  mbed::PwmOut r(PIN_R);
+  mbed::PwmOut g(PIN_G);
+  mbed::PwmOut b(PIN_B);
+  Serial btInput(PIN_BLE_TX, PIN_BLE_RX);
+
+/// Get random number from 0 to 1
+inline float random_number()
+{
+    return (rand()/(float(RAND_MAX)));
+}
+
+/// Create lightning effects
+void lightning(void const *args)
+{
+    while(1){
+        wait(3*pow(random_number(),3));
+        r= 0.8;
+        g= 0.8;
+        b= 0.9;
+        wait(0.2 * random_number());
+        r= 0;
+        g= 0;
+        b= 0;
+    }
+    
+
+}
+
 /// \brief Die in main.
 void __attribute__((noreturn)) die()
 {
@@ -29,6 +57,8 @@ void __attribute__((noreturn)) die()
   } while (true);
 }
 
+
+
 } // namespace
 
 // ====================== Global Definitions =========================
@@ -36,13 +66,34 @@ void __attribute__((noreturn)) die()
 int
 main()
 {
-  mbed::PwmOut r(PIN_R);
-  mbed::PwmOut g(PIN_G);
-  mbed::PwmOut b(PIN_B);
-
-  r = 1.0;
-  g = 0.1;
-  b = 0.5;
-
-  die();
+  char bnum = 0;
+  int mode = 0;
+  while(1){
+    if (btInput.getc() == '!'){
+        if (btInput.getc()=='B') { //button data
+        bnum = btInput.getc(); //button number
+        if ((bnum>='1')&&(bnum<='4')) //is a number button 1..4
+            mode=btInput.getc()-'0'; //turn on/off that num LED
+        }
+    }
+  
+    switch(mode){
+        case 1:
+        {
+            Thread thread(lightning);
+            wait(10);
+            thread.terminate();
+        }
+            break;
+        case 2:
+            die();
+            break;
+        case 3:
+            die();
+            break;
+        case 4:
+            die();
+            break;
+    }
+  }
 }
